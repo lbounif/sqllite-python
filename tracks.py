471153e0fdfc03e60afcb23949433b9eb4b10b51
Bounif Lynda <lynda.bounif@imadrassa.com>
2020-07-03 14:44:15 +0100

    create a lookup function

--- tracks.py
+++ tracks.py
@@ -37,3 +37,11 @@ CREATE TABLE Track (
 );
 ''')
 
+def lookup(d, key):
+    found = False
+    for child in d:
+        if found : return child.text
+        if child.tag == 'key' and child.text == key :
+            found = True
+    return None
+
